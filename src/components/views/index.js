97dd623ff2f0d644cdfdf1766fbf632d7dc32913
Caitlin Sangcap <csangcap405@gmail.com>
2021-05-22 00:43:05 -0400

    modified to add AllStudentsView and StudentsView to the list

--- src/components/views/index.js
+++ src/components/views/index.js
@@ -2,3 +2,5 @@
 export { default as AllCampusesView } from "./AllCampusesView";
 export { default as CampusView } from "./CampusView";
 export { default as HomePageView } from "./HomePageView";
+export { default as AllStudentsView} from "./AllStudentsView";
+export { default as StudentsView} from "./StudentsView";
