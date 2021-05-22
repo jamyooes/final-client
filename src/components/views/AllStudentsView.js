99fbc35b7b7a1af3509b4be235d14e87bc1782fd
ifteASC4 <ifteahmed2000@gmail.com>
2021-05-22 17:54:35 -0400

    added stuff to all students view

--- src/components/views/AllStudentsView.js
+++ src/components/views/AllStudentsView.js
@@ -34,11 +34,13 @@ const AllStudentsView = (props) => {
     <div>
       {props.allStudents.map((student) => (
         <div key={student.id}>
-          <Link to={`/student/${student.id}`}>
+          <Link to={`/students/${student.id}`}>
             <h1>{student.firstname} {student.lastname}</h1>
           </Link>
-          <p>{student.email}</p>
-          <p>{student.gpa}</p>
+          <p>Email: {student.email}</p>
+          <p>GPA: {student.gpa}</p>
+
+        <button onClick={() => props.deleteStudent(student.id)}>Delete </button>
         </div>
       ))}
     </div>
