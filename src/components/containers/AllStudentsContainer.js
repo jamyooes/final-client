bb32e4f3e1efa907d48ba9893f6b684ffe573c04
Caitlin Sangcap <csangcap405@gmail.com>
2021-05-22 00:44:10 -0400

    just adding small comments for myself to remember things

--- src/components/containers/AllStudentsContainer.js
+++ src/components/containers/AllStudentsContainer.js
@@ -1,7 +1,44 @@
-const AllStudentsContainer = () => {
-  return (
-    <h1>All Students View</h1>
-  );
+import { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { fetchAllStudentsThunk } from "../../store/thunks";
+import  AllStudentsView  from "../views/AllStudentsView";
+
+class AllStudentsContainer extends Component {
+  componentDidMount() {
+    console.log(this.props);
+    this.props.fetchAllStudents();
+  }
+
+  render() {
+    // pass all the info back to AllStudentsView to handle the displaying
+    return (
+      <AllStudentsView
+        allStudents={this.props.allStudents}
+      />
+    );
+  }
+}
+
+// Map state to props;
+const mapState = (state) => {
+  return {
+    allStudents: state.allStudents,
+  };
 };
 
-export default AllStudentsContainer;
+// Map dispatch to props;
+const mapDispatch = (dispatch) => {
+  return {
+    fetchAllStudents: () => dispatch(fetchAllStudentsThunk()),
+  };
+};
+
+// Type check props;
+AllStudentsContainer.propTypes = {
+  allStudents: PropTypes.array.isRequired,
+  fetchAllStudents: PropTypes.func.isRequired,
+};
+
+// Export our store-connected container by default;
+export default connect(mapState, mapDispatch)(AllStudentsContainer);
