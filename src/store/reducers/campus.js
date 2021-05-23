8f4c036f3a60acb58ab8925a47e6a0a6c3b7afec
jamyooes <james.yoo715@gmail.com>
2021-05-22 21:50:32 -0400

    fixed campusView

--- src/store/reducers/campus.js
+++ src/store/reducers/campus.js
@@ -5,14 +5,18 @@ import {
   FETCH_CAMPUS,
 } from "../actions/actionTypes";
 
-const campus = (state = [], action) => {
+const initialState = {
+  students: [],
+};
+
+const campus = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_CAMPUS:
       return action.payload;
     case ADD_CAMPUS:
       return [...state, action.payload];
     case DELETE_CAMPUS:
-      return state.filter((campus) => campus.id !== action.payload);
+      return state.filet((campus) => campus.id !== action.payload);
     case EDIT_CAMPUS:
       return state.map((campus) => {
         return campus.id === action.payload.id ? action.payload : campus;
