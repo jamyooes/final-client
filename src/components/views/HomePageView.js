364c82caa12f21062d8d175009933906843aebf0
ARGunawan <44307331+ARGunawan@users.noreply.github.com>
2021-05-23 18:19:09 -0400

    added some styling to home page

--- src/components/views/HomePageView.js
+++ src/components/views/HomePageView.js
@@ -26,6 +26,21 @@ const useStyles = makeStyles((theme) => ({
   links: {
     textDecoration: "none",
   },
+  text:{
+    textAlign: "center",
+    fontType: "bold",
+    fontFamily: "Courier, sans-serif",
+    color: "Black",
+    backgroundColor: "white",
+    fontSize: "18px",
+  },
+
+  images: {
+    display: "block",
+    marginleft: "auto",
+    marginright: "auto",
+    width: "50%",
+  },
 }));
 
 const HomePageView = () => {
@@ -35,6 +50,13 @@ const HomePageView = () => {
       <div className={classes.greeting}>
         <h1>Home Page</h1>
       </div>
+      <p> <img src="https://images.unsplash.com/photo-1590479773265-7464e5d48118?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"></img>
+      </p>
+      <p className = {classes.text}> Welcome to our Campus Managment website! With our website you can view all
+      the available databases already present, add new campuses, delete campuses, add new students, and see all existing
+      students in their respective schools.
+       </p>
+      <br />
     </div>
   );
 };
--- src/components/views/UnderConstruction.js
+++ src/components/views/UnderConstruction.js
@@ -33,7 +33,6 @@ const OofsView = () => {
   return (
     <div className={classes.root}>
       <div className={classes.greeting}>
-        <h1>RIP</h1>
         <img src="https://images.unsplash.com/photo-1590479773265-7464e5d48118?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"></img>
       </div>
     </div>
